bf3a05f620815761db14b223afa4c810976e830f
Anran Niu <anran.niu@asb.co.nz>
2021-06-18 09:09:53 +1200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'; 
+import {Vault} from "ansible-vault"
+import {TextField, Button, TextareaAutosize, FormGroup, Checkbox, FormControlLabel} from '@material-ui/core';
+
 
 function App() {
+  const [password, setPassword]=useState("")
+  const [plaintext, setPlaintext] = useState("")
+  const [cryptext, setCryptext] = useState("")
+  const [base64, setBase64] = useState(false)
+  const runEncrypt = () => {
+    const v = new Vault({password: password})
+    v.encrypt(plaintext).then(setCryptext)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+      <TextField required variant="outlined" type="password" label="Password" value={password} onChange={e => setPassword(e.target.value)}/>
+      <TextField required variant="outlined" label="Plaintext" type="text" value={plaintext} onChange={e => setPlaintext(e.target.value)}/>
+      <div>
+      <FormControlLabel
+        control={<Checkbox
+          checked={base64}
+          onChange={e => setBase64(e.target.checked)}
+          inputProps={{ 'aria-label': 'primary checkbox' }}
+        />}
+        label="Base64 encode before encrypting"
+      />
+       
+             <Button onClick={() => runEncrypt()} color="primary">Encrypt</Button>
+       </div>
+      <FormGroup>
+      <TextareaAutosize rows="5" type="text" disabled value={cryptext}/>
+      </FormGroup>
+      </form>
     </div>
   );
 }
